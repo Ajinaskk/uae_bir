4b156ea084a48a2441970154d37b476c7f94db76
Ajinas k k <ajinas1597@gmail.com>
2020-08-30 21:14:46 +0530

    first commit

---
+++ js/jsmore.js
@@ -0,0 +1,31 @@
+function morefounder() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+function morefoundation() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("mor");
+  var btnText = document.getElementById("myBt");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
